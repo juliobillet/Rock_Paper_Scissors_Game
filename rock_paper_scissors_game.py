6a9aac10c9d0f783eb058929479336e92ec4e1e1
Júlio Billet <76001545+juliobillet@users.noreply.github.com>
2021-09-17 08:22:56 -0300

    Update rock_paper_scissors_game.py

--- rock_paper_scissors_game.py
+++ rock_paper_scissors_game.py
@@ -1,4 +1,5 @@
 import random
+import os
 import sys
 
 rock = '''
@@ -35,6 +36,7 @@ rps_options = [rock, paper , scissors]
 print("Welcome to the Rock Paper Scissors Game!")
 players_choice = input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors:\n")
 
+# Start verifying if the player typed a valid number, and if so, print the corresponding ASCII Art:
 if players_choice.count(".") == True:
   print("Invalid option.\nPlease restart the game and choose a valid option!\nThe options are: 0 for Rock, 1 for Paper or 2 for Scissors.")
   sys.exit()
@@ -45,6 +47,7 @@ if int(players_choice) >= len(rps_options) or int(players_choice) < 0:
 else:
   print(rps_options[int(players_choice)])
 
+# Start of the logic to make the computer choose a random option and then compare the player's choice to the computer's choice and evaluate who is going to win.
 possible_options = [0, 1, 2]
 computer_choice = random.choice(possible_options)
 print(f"Computer's choice:\n{rps_options[computer_choice]}")
